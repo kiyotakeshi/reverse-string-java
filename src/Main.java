ab98c13ef3388df6fac8d543ea62448f70b859d0
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-29 14:39:06 +0900

    init commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String original = "katsudon";
+        int length = original.length();
+        // System.out.println(length); // 7
+
+        String[] ab = original.split("");
+        // System.out.println(ab[0]);
+        List<String> reverse = new ArrayList<>();
+
+        for (int i = 1; i <= original.length(); i++){
+            // System.out.println(ab[length - i]);
+            reverse.add(ab[length - i]);
+        }
+
+        String reverseString = "";
+        for (String s: reverse){
+            reverseString += s;
+        }
+
+        System.out.println(reverseString); // nodustak
+    }
+}
